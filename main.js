331b6bdfa04a05d1a00d72c202de39bc210157a1
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-03 15:36:40 +0200

    button function 1

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-let Fname = prompt ('Enter your first Name :');
-let surname = prompt ('Enter your Surname :');
-let citizenship = prompt ('Enter your Citizenship :');
-let age = Number(prompt ('Enter your age :'));
 
-if ( age >= 18 && citizenship =='South Africa'){
-   alert ('Hi '+ Fname+''+ surname +''+ ' you can vote')}
-else {
-   alert ('Hi '+ Fname+''+ surname +''+ ' you cant vote')
-}
 
 
